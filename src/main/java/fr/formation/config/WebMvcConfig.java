08956a231ccecba87eaf83657fcd2874ab20e90c
Zhybouck <48983661+Zhybouck@users.noreply.github.com>
2019-04-24 09:59:17 +0200

    Merge avec l'intercepteur et les mdp

--- src/main/java/fr/formation/config/WebMvcConfig.java
+++ src/main/java/fr/formation/config/WebMvcConfig.java
@@ -20,7 +20,7 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	SessionInterceptor sessionInterceptor;
     // Static Resource Config
     // equivalents for <mvc:resources/> tags
-    @Override
+    @Override 
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(31556926);
         registry.addResourceHandler("/img/**").addResourceLocations("/img/").setCachePeriod(31556926);
